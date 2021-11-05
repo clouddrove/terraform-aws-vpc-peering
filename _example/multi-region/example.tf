39c35dd377aec25cfb7b6cb8c52b439f64ca32da
Bobby8249 <bobby.singh@clouddrove.com>
2021-11-05 19:24:00 +0530

    fix fmt

--- _example/multi-region/example.tf
+++ _example/multi-region/example.tf
@@ -7,9 +7,9 @@ module "vpc-peering" {
   name             = "vpc-peering"
   environment      = "prod"
   label_order      = ["environment", "name"]
-  requestor_vpc_id = "vpc-032b6f35554a15e04"
+  requestor_vpc_id = "vpc-0fd16d55f6338d305"
 
-  acceptor_vpc_id  = "vpc-0770ef28023d8c920"
+  acceptor_vpc_id  = "vpc-0d778f4497ccad77a"
   accept_region    = "ap-south-1"
   auto_accept      = false
 }
