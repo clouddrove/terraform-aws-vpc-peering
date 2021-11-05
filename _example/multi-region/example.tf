d00e641c4d2c49d1a59e3e14ada8906c543bcb22
Bobby8249 <bobby.singh@clouddrove.com>
2021-11-05 21:11:21 +0530

    Fix fmt and cross region peering

--- _example/multi-region/example.tf
+++ _example/multi-region/example.tf
@@ -7,9 +7,9 @@ module "vpc-peering" {
   name             = "vpc-peering"
   environment      = "prod"
   label_order      = ["environment", "name"]
-  requestor_vpc_id = "vpc-0fd16d55f6338d305"
+  requestor_vpc_id = "vpc-XXXXXXXXXXXXXXXXX"
 
-  acceptor_vpc_id = "vpc-0d778f4497ccad77a"
+  acceptor_vpc_id = "vpc-XXXXXXXXXXXXXX"
   accept_region   = "ap-south-1"
   auto_accept     = false
 }
