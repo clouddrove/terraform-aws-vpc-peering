984ca164decbad4cf75a9b128bb948dd36cf55a0
theprashantyadav <theprashantyadav7@gmail.com>
2022-04-08 16:53:38 +0530

    update github-action

--- _example/multi-region/example.tf
+++ _example/multi-region/example.tf
@@ -7,9 +7,9 @@ module "vpc-peering" {
   name             = "vpc-peering"
   environment      = "prod"
   label_order      = ["environment", "name"]
-  requestor_vpc_id = "vpc-0637f4cbec95bd3cb"
+  requestor_vpc_id = "vpc-0637xxxxxxxxxxxxx"
 
-  acceptor_vpc_id = "vpc-0eb3503962ae548a2"
+  acceptor_vpc_id = "vpc-0eb3xxxxxxxxxxxxx"
   accept_region   = "ap-south-1"
   auto_accept     = false
 }
