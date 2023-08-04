a4ec4243575e503f3504d220a0a59d716863f8da
yadavprakash <yadavprakash.it@gmail.com>
2023-08-04 17:44:52 +0530

    update module with latest changes

--- _example/multi-region/example.tf
+++ _example/multi-region/example.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-1"
+  region = "ap-south-1"
 }
 
 module "vpc-peering" {
@@ -7,9 +7,9 @@ module "vpc-peering" {
   name             = "vpc-peering"
   environment      = "prod"
   label_order      = ["environment", "name"]
-  requestor_vpc_id = "vpc-0637xxxxxxxxxxxxx"
+  requestor_vpc_id = "vpc-09b5a7ef11a1d58b1"
 
-  acceptor_vpc_id = "vpc-0eb3xxxxxxxxxxxxx"
-  accept_region   = "ap-south-1"
+  acceptor_vpc_id = "vpc-0b619d80a58b3be5a"
+  accept_region   = "eu-north-1"
   auto_accept     = false
 }
